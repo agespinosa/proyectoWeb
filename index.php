06bc14e6c7cd88c2de9b034dc7c12e40902475b3
Gustavo Espinosa <age.espinosa@gmail.com>
2015-08-02 22:12:28 -0300

    saco los achivos includes de php para que el front no se encuentre acoplado a estos tipos de archivos.-

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
 
 <!DOCTYPE html>
-<html lang="es">
+<html lang="es" ng-app="routerApp">
     <head>
         <meta charset=utf-8>
         <title>Inicio </title>
-        <?php include 'inc/head_common.php'; ?>
+
+        <link rel="stylesheet" href="css/estilos.css">
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+        <script src="js/jquery-1.11.3.min.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+
+        <!-- JS (load angular, ui-router, and our custom js file) -->
+        <script src="http://code.angularjs.org/1.2.13/angular.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.8/angular-ui-router.min.js"></script>
+        <script src="js/app.js"></script>
     </head>  
     <!-- apply our angular app to our site -->
-    <body ng-app="routerApp">
-
-    <?php include 'inc/header_common.php';?>  
-
-        <!-- MAIN CONTENT -->
-        <div class="container">
-            <!-- THIS IS WHERE WE WILL INJECT OUR CONTENT ============================== -->
+    <body >
+        <div class="container-fluid">
+            <div ng-include="'templates/nav.html'"></div>
             <div ui-view></div>
+            <div ng-include="'templates/pie.html'"></div>
         </div>
-
-    <?php include 'inc/footer.php'; ?>
-    <div>
-        
-    </div>
-    
-</body>
+    </body>
 </html>
